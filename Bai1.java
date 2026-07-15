aa04f41cac7d668c80bdaeb11dcb392a3de47000
TienDat2404 <ntdat24104@gmail.com>
2026-07-15 08:00:34 +0700

    Add Bai1 - Bang cuu chuong cua 2

---
+++ Bai1.java
@@ -0,0 +1,8 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        System.out.println("Bảng cửu chương của 2");
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
